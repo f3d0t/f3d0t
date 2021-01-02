502e97d1e7b407168177d504d2d8ea7c8de3bf41
f3d0t <logrus7@gmail.com>
2021-01-03 00:38:24 +0300

    Add memory pair game

---
+++ memory_pair_game/app.js
@@ -0,0 +1,109 @@
+const CARDS_ARRAY = [
+	{
+		id: 1,
+		path: "img/1.png",
+	},
+	{
+		id: 2,
+		path: "img/2.png",
+	},
+	{
+		id: 3,
+		path: "img/3.png",
+	},
+	{
+		id: 4,
+		path: "img/4.png",
+	},
+	{
+		id: 5,
+		path: "img/5.png",
+	},
+	{
+		id: 6,
+		path: "img/6.png",
+	},
+	{
+		id: 7,
+		path: "img/7.png",
+	},
+	{
+		id: 8,
+		path: "img/8.png",
+	},
+];
+const CARDS_CONTAINER = document.getElementById("cards_container");
+const TURN_DELAY = 970;
+const openedNow = [];
+let openedPairsCount = 0;
+let startTime = Date.now();
+
+const shuffleArray = (array) => array.sort(() => 0.5 - Math.random());
+
+const getCardHTML = (cardObject) => {
+	const card = document.createElement("div");
+	card.classList.add("card");
+	card.setAttribute("data-id", cardObject["id"]);
+	const cardFront = document.createElement("div");
+	cardFront.classList.add("card__front");
+	const cardBack = document.createElement("div");
+	cardBack.classList.add("card__back");
+	cardBack.style.backgroundImage = `url(${cardObject["path"]})`;
+	card.append(cardFront, cardBack);
+	return card;
+};
+
+const getAllCardsHTML = (cardsArray) => {
+	const container = document.createDocumentFragment();
+	const cardsShuffledArray = shuffleArray([...cardsArray, ...cardsArray]);
+	for (let i = 0; i < 16; i++) {
+		container.append(getCardHTML(cardsShuffledArray[i]));
+	}
+	return container;
+};
+
+const checkPair = (twoCardsArray) => {
+	if (twoCardsArray[0].dataset.id == twoCardsArray[1].dataset.id) {
+		twoCardsArray.forEach((card) => {
+			setTimeout(() => {
+				card.classList.add("solved");
+			}, TURN_DELAY);
+		});
+		openedPairsCount++;
+	}
+	twoCardsArray.forEach((card) => {
+		setTimeout(() => {
+			card.classList.remove("flipped");
+		}, TURN_DELAY);
+	});
+};
+
+const startGame = () => {
+	openedPairsCount = 0;
+	CARDS_CONTAINER.innerHTML = "";
+	CARDS_CONTAINER.append(getAllCardsHTML(CARDS_ARRAY));
+    startTime = Date.now();
+};
+
+const initGame = () => {
+	startGame();
+	CARDS_CONTAINER.addEventListener("click", function ({ target }) {
+		const cardClicked = target.closest(".card:not(.flipped):not(.solved)");
+		if (cardClicked) {
+			cardClicked.classList.add("flipped");
+			openedNow.push(cardClicked);
+			if (openedNow.length == 2) {
+				checkPair(openedNow);
+				openedNow.splice(0, 2);
+				if (openedPairsCount == 8) {
+                    alert("You win!!!\nYour time is " + Math.floor((Date.now() - startTime) / 1000) + " seconds");
+					setTimeout(startGame(), 2000);
+				}
+			}
+		}
+	});
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+	initGame();
+});
